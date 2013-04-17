64b8bfd7d9ddb0cb141da256f995d4f4d682528c
Novikov Sergey <svnovikov@gmail.com>
2013-04-18 00:42:42 +0400

    Add node & redis base classes

---
+++ lib/ReachMedia/node.pm
@@ -0,0 +1,59 @@
+package ReachMedia::node;
+
+use strict;
+use ZMQ;
+use ZMQ::Constants qw(:all);
+use Data::MessagePack;
+use Data::Dumper;
+
+sub new
+{
+	my($class) = shift;
+	my $self =
+	{
+		_name => shift,
+		_mp => new Data::MessagePack,
+	};
+	bless $self, $class;
+    return $self;
+}
+
+sub connect
+{
+	my($self, $socket) = @_;
+	$self->{_cxt} = new ZMQ::Context or die( "Error creating ZMQ Context!" );
+    $self->{_sock} = $self->{_cxt}->socket( ZMQ_DEALER ) or die( "Error creating ZMQ Socket!" );
+    die( "Error connecting to server!" )
+        if $self->{_sock}->connect( $socket ) != 0;
+
+    my $msg = new ZMQ::Message( $self->{_mp}->pack( {
+                                "command"=>"introduce",
+                                "debug"=>1,
+                                "name"=>$self->{_name},
+                                "pid"=>$$
+                            } ) );
+    $self->{_sock}->sendmsg( $msg );
+    $msg->close();
+}
+
+sub run
+{
+	my($self) = @_;
+    while(1) {
+        my $msg = $self->{_sock}->recvmsg();
+        my $cmd = $self->{_mp}->unpack( $msg->data() );
+        $msg->close();
+
+        unless( exists $cmd->{command} ) {
+            printf( "No commands in message!\n" );
+            next;
+        }
+
+    #
+    # Тут обрабатываем запрос, находящийся $cmd
+    #
+
+    }
+}
+
+1;
